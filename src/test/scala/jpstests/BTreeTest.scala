0db58e72efff60aea66adb8fbe028417d44b40bd
Grzegorz Nowacki <grzegorznowack@gmail.com>
2018-05-15 13:10:29 +0200

    Test fix. Generics missing

--- src/test/scala/jpstests/BTreeTest.scala
+++ src/test/scala/jpstests/BTreeTest.scala
@@ -7,9 +7,7 @@ import jps._
 class BTreeTest extends FunSuite with BeforeAndAfter {
 
   test("get empty tree") {
-    val bTree = new BTree[Int, Int](Node[Int, Int](Vector.empty, Vector.empty), 1)
-
-//    val bTree = BTree.getEmptyBTree(1)    //order?
+    val bTree = BTree.getEmptyBTree[Int, Int](1)    //order?
 
     assert(bTree.rootNode.nodeElements.isEmpty)
     assert(bTree.rootNode.nodeChildren.isEmpty)
